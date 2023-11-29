648b3f3dd69571d85975f0ad6cf8fcec031f693a
Steve Frank <lardcanoe@gmail.com>
2023-11-29 08:40:08 -0500

    Rely on session map to determine if user exists

--- lib/housing_app_web/live_user_auth.ex
+++ lib/housing_app_web/live_user_auth.ex
@@ -6,25 +6,22 @@ defmodule HousingAppWeb.LiveUserAuth do
   import Phoenix.Component
   use HousingAppWeb, :verified_routes
 
-  def on_mount(:live_user_optional, _params, _session, socket) do
-    if socket.assigns[:current_user] do
+  def on_mount(:live_user_optional, _params, %{"user" => user}, socket) do
+    if user do
       {:cont, socket}
     else
       {:cont, assign(socket, :current_user, nil)}
     end
   end
 
-  def on_mount(:live_user_required, _params, _session, %{assigns: %{current_user: current_user}} = socket) when is_nil(current_user) do
-    {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
-  end
-
-  def on_mount(:live_user_required, _params, _session, %{assigns: %{current_user: current_user}} = socket) do
+  def on_mount(:live_user_required, _params, %{"user" => user}, %{assigns: %{current_user: current_user}} = socket) when is_binary(user) do
     case HousingApp.Accounts.get_default_user_tenant_for!(current_user.id) do
       nil ->
         {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
 
       user_tenant ->
         # Replicate assigns in lib/housing_app_web/controllers/auth_controller.ex
+        # But do NOT copy them, perform our own database queries
 
         Ash.set_tenant("tenant_#{user_tenant.tenant_id}")
 
@@ -37,11 +34,15 @@ defmodule HousingAppWeb.LiveUserAuth do
     end
   end
 
+  def on_mount(:live_user_required, _params, %{"user" => user}, socket) when is_nil(user) do
+    {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
+  end
+
+  def on_mount(:live_no_user, _params, %{"user" => user}, socket) when is_binary(user) do
+    {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/")}
+  end
+
   def on_mount(:live_no_user, _params, _session, socket) do
-    if socket.assigns[:current_user] do
-      {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/")}
-    else
-      {:cont, assign(socket, :current_user, nil)}
-    end
+    {:cont, assign(socket, :current_user, nil)}
   end
 end
