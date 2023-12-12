c7a5fe5097dc30a1b52775df3772d6eb8a213b3a
Steve Frank <lardcanoe@gmail.com>
2023-12-11 23:03:16 -0500

    Switch profiles to AgGrid

--- assets/js/hooks/ag-grid.js
+++ assets/js/hooks/ag-grid.js
@@ -5,6 +5,20 @@ import agGrid from "../../vendor/ag-grid-community.min";
 export default {
     mounted() {
         this.pushEvent("load-data", {}, (reply, ref) => {
+
+            reply.columns.forEach(c => {
+                if (c.link) {
+                    c.cellRenderer = function (params) {
+                        // target="_blank"
+                        return '<a href="' + params.value + '" rel="noopener">' + c.link + '</a>'
+                    }
+                } else if (c.email) {
+                    c.cellRenderer = function (params) {
+                        return '<a href="mailto:' + params.value + '" target="_blank" rel="noopener">' + params.value + '</a>'
+                    }
+                }
+            });
+
             const gridOptions = {
                 defaultColDef: {
                     wrapText: true,
@@ -18,6 +32,9 @@ export default {
                         // specify we want to use the date filter
                         filter: 'agDateColumnFilter'
                     }
+                },
+                autoSizeStrategy: {
+                    type: 'fitGridWidth',
                 },
                 columnDefs: reply.columns,
                 rowData: reply.data
--- lib/housing_app_web/live/profiles/index.ex
+++ lib/housing_app_web/live/profiles/index.ex
@@ -5,55 +5,84 @@ defmodule HousingAppWeb.Live.Profiles.Index do
 
   def render(%{live_action: :index} = assigns) do
     ~H"""
-    <h1 class="mb-4 text-2xl font-bold text-gray-900 dark:text-white">Profiles</h1>
+    <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-900 sm:rounded-lg">
+      <div class="flex flex-col items-start justify-between p-2 space-y-3 dark:bg-gray-800 md:flex-row md:items-center md:space-y-0 md:space-x-4">
+        <div class="flex items-center">
+          <h1 class="mr-3 text-2xl font-bold dark:text-white">Profiles</h1>
+        </div>
+        <div class="flex flex-row items-center justify-end flex-shrink-0 w-auto space-x-3">
+          <button
+            type="button"
+            class="flex items-center justify-center flex-shrink-0 px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+          >
+            <.link patch={~p"/profiles/new"}>
+              Add profile
+            </.link>
+          </button>
+        </div>
+      </div>
+    </div>
 
-    <.table id="profiles" rows={@profiles} pagination={false} row_id={fn row -> "profiles-row-#{row.id}" end}>
-      <:button>
-        <svg
-          class="h-3.5 w-3.5 mr-2"
-          fill="currentColor"
-          viewbox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-        >
-          <path
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-          />
-        </svg>
-        <.link patch={~p"/profiles/new"}>
-          Add profile
-        </.link>
-      </:button>
-      <:col :let={profile} :if={@current_user.role == :platform_admin} label="id">
-        <%= profile.id %>
-      </:col>
-      <:col :let={profile} label="User Name">
-        <.link patch={~p"/profiles/#{profile.id}/edit"}><%= profile.user_tenant.user.name %></.link>
-      </:col>
-      <:col :let={profile} label="User Email">
-        <%= profile.user_tenant.user.email %>
-      </:col>
-      <:action :let={profile}>
-        <.link
-          patch={~p"/profiles/#{profile.id}/edit"}
-          class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-        >
-          Edit
-        </.link>
-      </:action>
-    </.table>
+    <div class="dashboard-area-height mt-2">
+      <div id="myGrid" style="width: 100%; height: 100%;" class="ag-theme-quartz-dark" phx-hook="AgGrid"></div>
+    </div>
     """
   end
 
-  def mount(_params, _session, %{assigns: %{current_user_tenant: current_user_tenant, current_tenant: tenant}} = socket) do
-    {:ok, profiles} = HousingApp.Management.Profile.list(actor: current_user_tenant, tenant: tenant)
-
-    {:ok, assign(socket, profiles: profiles, sidebar: :profiles, page_title: "Profiles")}
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, sidebar: :profiles, page_title: "Profiles")}
   end
 
   def handle_params(params, _url, socket) do
     {:noreply, assign(socket, params: params)}
+  end
+
+  def handle_event(
+        "load-data",
+        %{},
+        %{assigns: %{current_user_tenant: current_user_tenant, current_tenant: tenant}} = socket
+      ) do
+    profiles =
+      HousingApp.Management.Profile.list!(actor: current_user_tenant, tenant: tenant)
+      |> Enum.sort_by(& &1.user_tenant.user.name)
+      |> Enum.map(fn p ->
+        if current_user_tenant.user.role == :platform_admin do
+          %{
+            "id" => p.id,
+            "name" => p.user_tenant.user.name,
+            "email" => p.user_tenant.user.email,
+            "edit" => ~p"/profiles/#{p.id}/edit"
+          }
+        else
+          %{
+            "name" => p.user_tenant.user.name,
+            "email" => p.user_tenant.user.email,
+            "edit" => ~p"/profiles/#{p.id}/edit"
+          }
+        end
+      end)
+
+    if current_user_tenant.user.role == :platform_admin do
+      {:reply,
+       %{
+         columns: [
+           %{field: "id", minWidth: 120},
+           %{field: "name", minWidth: 160},
+           %{field: "email", email: true, minWidth: 160},
+           %{field: "edit", link: "Edit", maxWidth: 90, filter: false, editable: false, sortable: false}
+         ],
+         data: profiles
+       }, socket}
+    else
+      {:reply,
+       %{
+         columns: [
+           %{field: "name", minWidth: 160},
+           %{field: "email", email: true, minWidth: 160},
+           %{field: "edit", link: "Edit", maxWidth: 90, filter: false, editable: false, sortable: false}
+         ],
+         data: profiles
+       }, socket}
+    end
   end
 end
